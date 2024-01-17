921d2916633e9f38d2fe07804937af0582067d9e
JimwillCapino <jimwillcapino@gmail.com>
2024-01-17 22:32:09 +0800

    Add project files.

---
+++ Basecode.Data/BasecodeContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basecode.Data
+{
+    public class BasecodeContext : IdentityDbContext<IdentityUser>
+    {
+        public BasecodeContext(DbContextOptions<BasecodeContext> options) : base(options)
+        { 
+            
+        }
+
+       
+    }
+}
